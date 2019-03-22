66ce5c9679a915e0c527d7f0dfb61c2882430e39
mingueyun <mingueyun@gmail.com>
2019-03-21 18:00:47 -0700

    added exercise for branching
    Uploaded exercise for branching for step 92 which had to calculate a shipment quote

---
+++ exerciseBranching/exerciseBranching/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciseBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            double dimensions = width + height + length;
+
+            double sumTotal = dimensions * weight / 100;
+            decimal quote = Convert.ToDecimal(sumTotal);
+
+            if (dimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            else
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: {0:c}\nThanks", quote);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exerciseBranching/exerciseBranching/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("exerciseBranching")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exerciseBranching")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72400340-3a6c-4ca6-8a3f-3068e4ecc95e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
