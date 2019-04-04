93fc5bea3c89317f53aa7e90a3c7312576c79203
mingueyun <mingueyun@gmail.com>
2019-04-04 15:30:23 -0700

    uploaded class and object excercise
    uploaded file for step 111

---
+++ classesObjects/classesObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesObjects
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            
+            Speed s = new Speed();
+            Program sp = new Program();
+
+
+            int twoSpeeds;
+            
+            Console.WriteLine("whats your current speed? ");
+            int currentSpeed = Convert.ToInt32(Console.ReadLine());
+            twoSpeeds = sp.MySpeed(currentSpeed, s.postedSpeed);
+            Console.WriteLine("you are going " + currentSpeed + " and the posted speed is " + s.postedSpeed);
+
+            double gasMileage;
+            Console.WriteLine("\nHow much miles did you drive? ");
+            double milesDriven = Convert.ToDouble(Console.ReadLine());
+            gasMileage = sp.Gas(milesDriven, s.startedMiles);
+            Console.WriteLine(" you have used " + gasMileage + " gallon of gas cause you have driven " +  milesDriven + " miles since you started at " + s.startedMiles + " miles.");
+
+
+            Console.ReadLine();
+      
+        }
+        public int MySpeed(int speed1, int speed2)
+        {
+            int fast;
+            if (speed1 > speed2)
+                fast = speed1;
+            else
+                fast = speed2;
+            return fast;
+
+        }
+
+        public double Gas ( double gas1, double gas2)
+        {
+            double burning;
+            if (gas1 > 0)
+                burning = gas1  / 30;
+            else
+                burning = gas1;
+            return burning;
+        }
+
+    }
+}
---
+++ classesObjects/classesObjects/Speed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesObjects
+{
+    //named a class called Speed
+    public class Speed
+    {
+        public int startedMiles { get; set; }
+        public int postedSpeed { get; set; }
+      
+
+        // made a constructor Speed  with  parameter of schoolzone and highspeed 
+        public Speed( )
+        {
+            startedMiles =   2000;
+            postedSpeed =  60;
+        }
+
+
+    }
+}
