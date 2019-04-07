26f03c6500692ef824837ffef1d739917738f714
mingueyun <mingueyun@gmail.com>
2019-04-07 16:13:51 -0700

    updating voidmethod file
    updating to add all VS files

---
+++ voidMethod/voidMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethod
+{
+    class Program
+    {
+
+        
+        static void Main()
+        {
+            SeatBucket seat = new SeatBucket();
+            seat = Grab(seat);
+           
+            
+
+          
+
+
+            
+            Console.ReadLine();
+
+
+        }
+        public static  SeatBucket Grab(SeatBucket seat)
+        {
+            int score;
+            Console.WriteLine("Enter your  available seats\n ");
+            int firstMathScore = Convert.ToInt32(Console.ReadLine());
+            score = seat.Add(firstMathScore, 12);
+            return seat;
+
+     
+        }
+
+    }
+
+    
+}
+
+
---
+++ voidMethod/voidMethod/SeatBucket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethod
+{
+      class SeatBucket
+    {
+    
+        
+        public int Add( int a,  int b)
+        {
+            int adding;
+
+            if (a == b)
+            {
+                adding = a + b;
+                Console.WriteLine("sum of the two " + adding);
+            }
+            else
+            {
+                adding = a % b;
+                Console.WriteLine("remaining " + adding);
+
+            }
+            return adding;
+
+
+
+
+
+        }
+
+    }
+}
