4cf53f9ff9c9f9dd0f015c6743c1b1fa646d005a
mingueyun <mingueyun@gmail.com>
2019-04-07 16:16:40 -0700

    uploading  step 114
    uploading step 114

---
+++ MoreMethodsExercise/moreMethodsExercises/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethodsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathStuff mathstuff = new MathStuff();
+            mathstuff = DoStuff(mathstuff);
+
+
+
+            
+            
+
+
+            Console.ReadLine();
+
+        }
+          public static MathStuff DoStuff(MathStuff mathstuff )
+        {
+            int score;
+            Console.WriteLine("Enter your  1st math score\n ");
+            int firstMathScore = Convert.ToInt32(Console.ReadLine());
+            score = mathstuff.ValueType(firstMathScore, 12  );
+
+            double scores;
+            Console.WriteLine("Enter your  2nd math score\n ");
+            double secondtMathScore = Convert.ToDouble(Console.ReadLine());
+            scores = mathstuff.Value(secondtMathScore, 4);
+
+            int stuff;
+            Console.WriteLine(" tell me a number \n");
+            int tellMe = Convert.ToInt32(Console.ReadLine());
+            stuff = mathstuff.Joust(tellMe);
+
+
+
+            return mathstuff;
+
+
+        }
+    }
+}
---
+++ MoreMethodsExercise/moreMethodsExercises/mathStuff.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethodsExercises
+{
+    public class MathStuff
+    {
+ 
+
+        public int ValueType(int first, int second)
+        {
+
+            int point1;
+            if (first > second)
+            {
+                point1 = first * 10;
+                Console.WriteLine("its greater " + point1);
+
+            }
+            else
+            {
+                point1 = first + 10;
+                Console.WriteLine("its less " + point1);
+            }
+            return point1;
+
+
+        }
+        public double Value(double ssf, double stf)
+        {
+
+            double point2;
+            if (ssf < stf)
+            {
+                point2 = ssf + 10;
+                Console.WriteLine("its less " + point2);
+
+            }
+            else
+            {
+                point2 = ssf - 1;
+                Console.WriteLine("its greater " + point2);
+            }
+            return point2;
+            
+
+        }
+        public int Joust(int yikes )
+        {
+            int toke;
+            if (yikes > 3)
+            {
+                toke = yikes + 10;
+                Console.WriteLine(" work " + toke);
+
+            }
+            else
+            {
+                toke = yikes - 1;
+                Console.WriteLine("wont work " + toke);
+
+            }
+            
+         
+            
+
+            return toke;
+
+        }
+    }
+}
