12dccadb4d4dba5773b22fb21a52acc6b1e49d8f
mingueyun <mingueyun@gmail.com>
2019-04-08 15:55:14 -0700

    uploading step 117
    uploadding file called ClassMethods.
    added class named classy and added a method and instantiated in class program and made that static. i also overloaded classy with   a new class and method classnew2

---
+++ ClassMethods/ClassMethods/Classy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    class Classy
+    {
+        public void ClassyNew(out int num1)
+        {
+            Console.WriteLine("Enter a number ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + " divided by 2 is " + num1 / 2);
+        }      
+
+    }
+    public  class Classy2 
+    {
+        public int   ClassyNew2(int A, int B)
+        {
+            return A / B;
+
+        }
+        public float ClassyNew2(float A, float B)
+        {
+            return A - B;
+        }
+
+
+    }
+       
+
+
+
+        
+}
---
+++ ClassMethods/ClassMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    public static  class Program
+    {
+        static void Main(string[] args)
+        {
+            Classy ClueLess = new Classy();
+            Classy2 NotVlueLess = new Classy2();
+            int a;
+            
+            ClueLess.ClassyNew(out  a );
+
+            Console.WriteLine("the value of input is {0}\n", a);
+            Console.WriteLine(NotVlueLess.ClassyNew2(20, 2));
+            Console.WriteLine(NotVlueLess.ClassyNew2(4.6f, 2.3f));
+
+            Console.ReadLine();
+        }
+    }
+}
