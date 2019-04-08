41741dc7c2dbae170fcd6cb56e187378a8909604
mingueyun <mingueyun@gmail.com>
2019-04-08 12:21:02 -0700

    uploaded step 115
    Uploaded step 115 called methodNoNumber

---
+++ MethodNoNumber/methodNoNumber/NoNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodNoNumber
+{
+    class NoNumber
+    {
+
+
+
+
+        public void NoIntOption( int required, int optionalint  )
+        {
+            Console.WriteLine(required + optionalint);
+ 
+        }
+    }
+}
---
+++ MethodNoNumber/methodNoNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodNoNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// this block of code makes sure that no input doesnt throw an exception for 2nd input
+
+            NoNumber someNumber = new NoNumber();
+         
+            
+            Console.WriteLine("Enter a number 1 ");
+            int oneNum = int.Parse(Console.ReadLine( ));
+
+            int twoNum;
+            Console.WriteLine("Enter a number 2. if you dont know a number just push enter");
+            bool result = int.TryParse(Console.ReadLine(), out twoNum);
+            if (result)
+            {
+                someNumber.NoIntOption(oneNum, twoNum);
+            }
+            else
+            {
+                someNumber.NoIntOption(oneNum, 0);
+            }
+          
+     
+
+            Console.ReadLine();
+        }
+    }
+}
