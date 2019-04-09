1316306da526e9086b096ff0c7b95ca46cfea9c9
mingueyun <mingueyun@gmail.com>
2019-04-08 18:31:01 -0700

    uploading step 120
    uploading exercise for  class inheritance

---
+++ inheritanceClassExercise/InheritanceClassExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee badge = new Employee();
+            
+          
+            badge.FirstName = "Sample";
+            badge.LastName = "student";
+            
+
+            badge.SayName();
+            badge.Fired();
+            Console.ReadLine();
+        }
+    }
+}
