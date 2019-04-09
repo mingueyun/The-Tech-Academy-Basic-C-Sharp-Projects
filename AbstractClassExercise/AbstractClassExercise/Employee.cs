3a4c9d1ab2ef0d257d7c9d28d4e0a7a8dc3c106e
mingueyun <mingueyun@gmail.com>
2019-04-08 20:11:39 -0700

    uploading step 124
    uploading file abstractClassExercise

---
+++ AbstractClassExercise/AbstractClassExercise/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+     public class Employee : Person
+    {
+            
+        // implementing sayname() method from person class
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ AbstractClassExercise/AbstractClassExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.firstName = "Sample";
+            person.lastName = "student";
+            person.SayName();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
