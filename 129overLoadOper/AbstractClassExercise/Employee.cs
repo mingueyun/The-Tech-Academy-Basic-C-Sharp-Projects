5fa602cc4cadabdac4420261db4f414753632177
mingueyun <mingueyun@gmail.com>
2019-04-09 17:46:05 -0700

    uploading step 129
    uploaded file called 129overLoadOper
     I added  the properties to Employee. I thnk i forgot that in a previous step. then i added a bool  method in Person for an equality operator and  called it in programs.

---
+++ 129overLoadOper/AbstractClassExercise/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+     public class Employee : Person, IQuittable
+    {
+        public int id { get; set; }
+            
+        // implementing sayname() method from person class
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit(   )
+        {
+            tellAge( );
+
+        }
+    }
+}
---
+++ 129overLoadOper/AbstractClassExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee();
+            Employee two = new Employee();
+            one.id = 1;
+            one.firstName = "sam";
+            two.id = 2;
+            two.firstName = "sam";
+            Console.WriteLine(one.Equals(two));
+
+
+            //Employee person = new Employee();
+
+            //person.age = 6;
+            //person.tellAge();
+
+
+
+            //person.firstName = "Sample";
+            //person.lastName = "student";
+            //person.SayName();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
