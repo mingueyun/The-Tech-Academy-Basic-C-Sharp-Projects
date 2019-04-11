6c66a1f21842f39e880cbe9bd6b854979ddee172
mingueyun <mingueyun@gmail.com>
2019-04-10 22:33:47 -0700

    uploading step 137
    uploading file 137StrcutEx

---
+++ 137StructEx/137StructEx/Number.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _137StructEx
+{
+     public struct Number
+    {
+        public decimal Amount { get; set; }
+        public  string Name { get; set; }
+        public Number( string name, decimal amount)
+        {
+            Name = name;
+            Amount = amount;
+            
+        }
+        
+
+    }
+}
---
+++ 137StructEx/137StructEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _137StructEx
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            Number person1 = new Number("Min" , 50.0m);
+            Console.WriteLine(" person1 name is = {0} and debt  Amount = {1} ", person1.Name, person1.Amount);
+
+            Console.ReadLine();
+
+        }
+    }
+}
