345d4b52bb2083b142bde16ae4c31e953ec2b7e7
mingueyun <mingueyun@gmail.com>
2019-04-10 19:03:38 -0700

    uploading step 134
    uploading enum using a  try  catch block.

---
+++ 134EnumEx/134EnumEx/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _134EnumEx
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Weeks week = new Weeks();
+            week.DaysOfTheWeek = new DaysOfTheWeek();
+
+            Console.WriteLine("what day of the week is it? ");
+            string GuessDay = Console.ReadLine();
+
+
+            try
+            {
+                DaysOfTheWeek days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), GuessDay);
+                switch (GuessDay)
+                {
+                    case "Monday":
+                        Console.WriteLine("{0} = {1:D} ", GuessDay, Enum.Parse(typeof(DaysOfTheWeek), days.ToString()));
+                        break;
+                    case "Tuesday":
+                        Console.WriteLine("{0} = {1:D} ", GuessDay, Enum.Parse(typeof(DaysOfTheWeek), days.ToString()));
+                        break;
+                    default:
+                        Console.WriteLine("Please enter an actual day of the week. ");
+                        break;
+
+
+                }
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week. \n ", ex);
+
+
+                // testing it gives an exception it cant find the incorrectly spelled stuff
+                //DaysOfTheWeek days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), GuessDay);
+                //if (Enum.IsDefined(typeof(DaysOfTheWeek), days) | days.ToString().Contains(GuessDay))
+                //{
+                //    Console.WriteLine("{0} = {1:D} ", GuessDay, Enum.Parse(typeof(DaysOfTheWeek), days.ToString()));
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Please enter an actual day of the week. ");
+
+                //}
+
+
+
+
+
+
+                //this one keeps iterating and prints all 7 days and it prints values.
+                //foreach (string cname in Enum.GetNames(typeof(DaysOfTheWeek)))
+                //{
+
+                //    try
+                //    {
+                //        DaysOfTheWeek days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), GuessDay);
+                //        if (Enum.IsDefined(typeof(DaysOfTheWeek), days) | days.ToString().Contains(","))
+                //            Console.WriteLine("{0} = {1:D} ", GuessDay, Enum.Parse(typeof(DaysOfTheWeek), days.ToString()));
+                //        else
+                //            Console.WriteLine("Please enter an actual day of the week. ");
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        Console.WriteLine(ex);
+                //    }
+
+
+
+
+
+                // practice but it prints out all 7  but print the value a left or right bool. it doesnt work
+
+                //int vale = Convert.ToInt32(week.DaysOfTheWeek);
+                //foreach (var cname in Enum.GetNames(typeof(DaysOfTheWeek))) ;
+                //{
+                //    try
+                //    {
+
+                //        DaysOfTheWeek days = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), GuessDay);
+                //        if (Enum.IsDefined(typeof(DaysOfTheWeek), days) || days.ToString().Contains(","))
+                //            Console.WriteLine("Your selection '{0}' is {1}.", GuessDay, days.ToString());
+                //        else
+                //            Console.WriteLine("Please enter an actual day of the week. ");
+                //    }
+
+
+                //    catch (Exception ex)
+                //    {
+                //        Console.WriteLine(ex);
+                //    }
+
+                //}
+
+
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ 134EnumEx/134EnumEx/Weeks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _134EnumEx
+{
+    class Weeks
+    {
+        public DaysOfTheWeek DaysOfTheWeek { get; set; }
+    }
+    public enum DaysOfTheWeek
+    {
+        Monday =1,
+        Tuesday =2,
+        Wednesday =3,
+        Thursday =4,
+        Friday =5,
+        Saturday =6,
+        Sunday =7,
+    }
+
+}
