6350ca360b68839aad760ac950453d9d392f7299
mingueyun <mingueyun@gmail.com>
2019-04-16 16:12:32 -0700

    uploading step 156
    Uploading file 156dateTime

---
+++ 156dateTimeEx/156dateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _156dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("give me number in hours you want to be reminded?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(x));
+
+
+       
+
+            Console.ReadLine();
+        }
+
+    }
+}
