caf2eba48dcc961e373c8ef1eab66d1012ffa4bf
mingueyun <mingueyun@gmail.com>
2019-04-16 15:21:56 -0700

    uploading file 154logFile
    Uploading for step 154

---
+++ 154logFileEx/154logFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _154logFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tell me a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Mingu\logs\number.txt", true))
+            {
+                file.WriteLine(answer);
+            }
+            using (StreamReader sr = new StreamReader(@"C:\Users\Mingu\logs\number.txt"))
+            {
+                string line = sr.ReadToEnd();
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+           
+                
+            
+        }
+        
+        
+    }
+}
