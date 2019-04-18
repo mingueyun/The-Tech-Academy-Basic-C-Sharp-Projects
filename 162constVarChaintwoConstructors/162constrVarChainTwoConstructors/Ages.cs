10fb7147e5fbfa6dfd15c25333cd6b6a67bde07a
mingueyun <mingueyun@gmail.com>
2019-04-17 17:16:03 -0700

    uploading step 162
    uploading file for step 162 called 162constrVarChainTwoConstructors

---
+++ 162constVarChaintwoConstructors/162constrVarChainTwoConstructors/Ages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _162constrVarChainTwoConstructors
+{
+    public class Ages
+    {
+
+
+        static Ages()
+        {
+            const string myName = "Min";
+            Console.WriteLine("Your first name is {0}", myName);
+
+        }
+        public Ages(int myAge, string mynames)
+        {
+            this.Age = myAge;
+            this.Name = mynames;
+
+        }
+           
+
+        public Ages(int Yearnow, int yearborn)
+        {
+            Age = Yearnow - yearborn;
+        }
+        public int Age { get; set; }
+        public string Name { get; set; }
+  
+
+    }
+}
---
+++ 162constVarChaintwoConstructors/162constrVarChainTwoConstructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _162constrVarChainTwoConstructors
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var ages = new Ages(2019, 1979);
+
+
+            Console.WriteLine("you are {0} years old", ages.Age);
+
+            Console.ReadLine(); 
+
+        }
+        
+        
+    }
+}
